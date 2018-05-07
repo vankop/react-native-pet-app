4704d27cd1352a8d642c16fa578916061c730f50
ivan.kopeykin <ivan.kopeykin@gmail.com>
2018-05-07 15:58:37 +0300

    feat(security/device): request permissions

--- src/security/device.js
+++ src/security/device.js
@@ -1,7 +1,6 @@
-import { Platform } from 'react-native';
+import { Platform, PermissionsAndroid } from 'react-native';
 import { version } from '../../package.json';
 
-const objectId = 'fa9b397bc680d6a607679bb3d83a0bcd178ff52a96754428b97c2523cee9eb04';
 const guid = 'ec8e0d95690aacd0c8f716d76741195b';
 
 export default class Device {
@@ -14,5 +13,24 @@ export default class Device {
             app_version: version,
             user_agent: Platform.OS.toUpperCase()
         };
+    }
+
+    static async requestLocationPermissions() {
+        if (Platform.OS === 'ios') {
+            // explicitly allow request permissions on every platform
+            return true;
+        } else if (Platform.Version >= 23) {
+            const granted = await PermissionsAndroid.request(
+                PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+                {
+                    title: 'React Native Pet App',
+                    message: 'React Native Pet App needs access to your current location.'
+                }
+            );
+
+            return granted === PermissionsAndroid.RESULTS.GRANTED;
+        }
+
+        return false;
     }
 }
